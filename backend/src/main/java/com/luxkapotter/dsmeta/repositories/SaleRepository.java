06a9973083c82962d15f70f9ba932b5b1c74ebbf
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-11-16 10:49:29 -0300

    criando primeiros passos da API REST

---
+++ backend/src/main/java/com/luxkapotter/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.luxkapotter.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luxkapotter.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
